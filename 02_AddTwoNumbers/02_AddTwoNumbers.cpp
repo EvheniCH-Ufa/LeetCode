2feb184ae815df14e6113afee1e08a13e2afadf6
Евгений Чернышёв <Ch.Ew.Se@yandex.ru>
2025-07-13 17:56:56 +0500

    Add files via upload

---
+++ 02_AddTwoNumbers/02_AddTwoNumbers.cpp
@@ -0,0 +1,206 @@
+﻿// 02_AddTwoNumbers.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        if (!l1) return l2;
+        if (!l2) return l1;
+
+        auto curr_first_ptr = l1;
+        auto curr_second_ptr = l2;
+
+        int perenos = 0;
+        int nomer_kruga = 0;
+        while (curr_first_ptr || curr_second_ptr)
+        {
+            ++nomer_kruga;
+
+            int val = perenos + ((curr_first_ptr) ? curr_first_ptr->val : 0) + ((curr_second_ptr) ? curr_second_ptr->val : 0);
+
+            curr_first_ptr->val = val % 10 ;
+            perenos = val / 10;
+
+
+            if (!curr_first_ptr->next && !curr_second_ptr)
+            {
+                break;
+            }
+
+
+            if (!curr_first_ptr->next && !curr_second_ptr->next)
+            {
+                break;
+            }
+
+            if ((curr_first_ptr->next && !curr_second_ptr)
+                || (curr_first_ptr->next && curr_second_ptr && !(curr_second_ptr->next)))
+            {
+                curr_first_ptr = curr_first_ptr->next;
+                curr_second_ptr = nullptr;
+                continue;
+            }
+
+            if (curr_first_ptr->next && curr_second_ptr->next)
+            {
+                curr_first_ptr = curr_first_ptr->next;
+                curr_second_ptr = curr_second_ptr->next;
+                continue;
+            }
+
+            if (!curr_first_ptr->next && curr_second_ptr->next) // first-next == null   and   second-next != null
+            {
+                curr_first_ptr->next = curr_second_ptr->next;
+                curr_first_ptr = curr_first_ptr->next;
+                curr_second_ptr = nullptr;
+                continue;
+            }
+
+        }
+
+        if (perenos)
+        {
+            curr_first_ptr->next = new ListNode(perenos);
+        }
+        return l1;
+    }
+};
+
+int main()
+{
+    {
+        ListNode* l1 = new ListNode(2);
+        l1->next = new ListNode(4);
+        l1->next->next = new ListNode(3);
+
+        ListNode* l2 = new ListNode(5);
+        l2->next = new ListNode(6);
+        l2->next->next = new ListNode(4);
+
+        Solution sol;
+
+        auto res = sol.addTwoNumbers(l1, l2);
+        while (res)
+        {
+            std::cout << res->val;
+            res = res->next;
+        }
+        std::cout << std::endl;
+
+        /*
+        Input: l1 = [2, 4, 3],
+               l2 = [5, 6, 4]
+        Output    : [7, 0, 8]
+        Explanation : 342 + 465 = 807.
+        */
+    }
+
+    {
+        ListNode* l1 = new ListNode(0);
+
+        ListNode* l2 = new ListNode(0);
+
+        Solution sol;
+
+        auto res = sol.addTwoNumbers(l1, l2);
+        while (res)
+        {
+            std::cout << res->val;
+            res = res->next;
+        }
+        std::cout << std::endl;
+
+        /*
+    Input : l1 = [0], l2 = [0]
+    Output : [0]
+    Example 3 :
+        */
+    }
+
+    {
+        ListNode* l1 = new ListNode(9);
+        auto c_l1 = l1;
+
+        c_l1->next = new ListNode(9);
+        c_l1 = c_l1->next;
+
+        c_l1->next = new ListNode(9);
+        c_l1 = c_l1->next;
+
+        c_l1->next = new ListNode(9);
+        c_l1 = c_l1->next;
+
+        c_l1->next = new ListNode(9);
+        c_l1 = c_l1->next;
+
+        c_l1->next = new ListNode(9);
+        c_l1 = c_l1->next;
+
+        c_l1->next = new ListNode(9);
+
+
+        ListNode* l2 = new ListNode(9);
+        auto c_l2 = l2;
+        c_l2->next = new ListNode(9);
+        c_l2 = c_l2->next;
+        c_l2->next = new ListNode(9);
+        c_l2 = c_l2->next;
+        c_l2->next = new ListNode(9);
+
+        Solution sol;
+
+        auto res = sol.addTwoNumbers(l1, l2);
+        while (res)
+        {
+            std::cout << res->val;
+            res = res->next;
+        }
+        std::cout << std::endl;
+
+        /*
+    Input : l1 = [9, 9, 9, 9, 9, 9, 9],
+            l2 = [9, 9, 9, 9]
+    Output     : [8, 9, 9, 9, 0, 0, 0, 1]
+        */
+    }
+
+    {
+        ListNode* l1 = new ListNode(2);
+        l1->next = new ListNode(4);
+        l1->next->next = new ListNode(9);
+
+        ListNode* l2 = new ListNode(5);
+        l2->next = new ListNode(6);
+        l2->next->next = new ListNode(4);
+        l2->next->next->next = new ListNode(9);
+
+        Solution sol;
+
+        auto res = sol.addTwoNumbers(l1, l2);
+        while (res)
+        {
+            std::cout << res->val;
+            res = res->next;
+        }
+        std::cout << std::endl;
+
+        /*
+        Input: l1 = [2, 4, 3],
+               l2 = [5, 6, 4, 9]
+        Output    : [7, 0, 4, 0, 1]
+        */
+    }
+
+
+
+}
---
+++ 03_LongestSubstringWithoutRepeatingCharacters/000003_LongestSubstringWithoutRepeatingCharacters.cpp
@@ -0,0 +1,83 @@
+﻿// 000003_LongestSubstringWithoutRepeatingCharacters.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(std::string s)
+    {
+        std::vector<int> symbols_in_curr_posled(256, -1); // po kodu simvola nomer v posled
+
+        std::string curr_posled;
+        int maxlen = 0;
+        for (char ch : s)
+        {
+            if (symbols_in_curr_posled[ch] >= 0)  // if curr_symbol exist in curr_posled
+            {
+                int curr_posled_size = curr_posled.size();
+                maxlen = std::max(maxlen, curr_posled_size);
+
+                int pos = symbols_in_curr_posled[ch];
+                for (int i = 0; i < curr_posled.size(); ++i)
+                {
+                    symbols_in_curr_posled[curr_posled[i]] -= (pos + 1);
+                }
+                curr_posled = curr_posled.substr(pos+1);
+            }
+            curr_posled += ch;
+            symbols_in_curr_posled[ch] = curr_posled.size() - 1;
+        }
+
+        int curr_posled_size = curr_posled.size();
+        return std::max(maxlen, curr_posled_size);
+    }
+};
+
+
+int main()
+{
+    Solution sol;
+
+    {
+        //Example 1:
+        //Input: s = "abcabcbb"
+        //Output : 3
+        //Explanation : The answer is "abc", with the length of 3.
+        std::cout << sol.lengthOfLongestSubstring("abcabcbb") << std::endl;
+    }
+
+
+
+    {
+        //    Example 2 :
+        //    Input: s = "bbbbb"
+        //    Output : 1
+        //    Explanation : The answer is "b", with the length of 1.
+        std::cout << sol.lengthOfLongestSubstring("bbbbb") << std::endl;
+    }
+    
+    
+    {
+        //    Example 3 :
+        //    Input: s = "pwwkew"
+        //    Output : 3
+        //    Explanation : The answer is "wke", with the length of 3.
+        //    Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+        std::cout << sol.lengthOfLongestSubstring("pwwkew") << std::endl;
+    }
+
+    {
+        //    Example 3 :
+        //    Input: s = "pwwkew"
+        //    Output : 3
+        //    Explanation : The answer is "wke", with the length of 3.
+        //    Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+        std::cout << sol.lengthOfLongestSubstring(" ") << std::endl;
+    }
+
+
+}
